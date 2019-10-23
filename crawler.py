fc118231bb47850c0ee503cfaa2bb3218eb653e9
Jesus Martinez <jesusd.martinez@Jesuss-MacBook-Pro.local>
2019-10-23 21:48:45 +0800

    Updated Crawler - Yet to Chart

--- crawler.py
+++ crawler.py
@@ -1,102 +1,81 @@
-from typing import Dict
 import requests
 import argparse
-import pprint
 from bs4 import BeautifulSoup
 import re
 import os.path
-import pprint
-
-extension_whitelist = ['.html', '.htm', '']
-href = []
-updated_list = []
-
-response = requests.get('https://en.wikipedia.org/wiki/Router_(computing)')
-plain_text = response.text
-soup = BeautifulSoup(plain_text, "html.parser")
-rate = soup.find('body')
+import queue
+import numpy as np
+from typing import Iterable, Tuple
+import matplotlib.pyplot as plt
+import networkx as nx
 
 
-for link in rate.findAll('a', attrs={'href': re.compile("^/wiki/")}):
-    href.append(link.get('href'))
-
-for x in href:
-    if os.path.splitext(x)[1][1:] in extension_whitelist:
-        updated_list.append(x)
+class CrawlerItem():
+    def __init__(self, url, depth):
+        self.url = url
+        self.depth = depth
 
 
+class Crawler():
+    def __init__(self, initial_url, max_depth):
+        self.initial_url = initial_url
+        self.max_depth = max_depth
+
+    def crawl(self):
+        result = list()
+
+        myqueue = queue.Queue()
+        myqueue.put(CrawlerItem(self.initial_url, 1))
+
+        while not myqueue.empty():
+            item = myqueue.get()
+            url = item.url
+
+            response = requests.get(url)
+            plain_text = response.text
+            soup = BeautifulSoup(plain_text, "html.parser")
+            rate = soup.find(id='bodyContent')
+
+            extension_whitelist = ['.html', '.htm', '']
+            new_list = list()
+            new_new_list = list()
+
+            for link in rate.findAll('a', attrs={'href': re.compile("^/wiki/")}):
+                new_list.append('https://en.wikipedia.org' + link.get("href"))
+
+            for link in new_list:
+                if os.path.splitext(link)[1] in extension_whitelist:
+                    new_new_list.append(link)
+
+            new_new_list = list(np.random.choice(new_list, 5, replace=False))
+
+            for link in new_new_list:
+                result.append((url, link))
+
+                if item.depth < self.max_depth:
+                    myqueue.put(CrawlerItem(link, item.depth + 1))
+
+        return result
+        # my_chart = nx.Graph
+        # my_chart.add_edges_from(result)
+        # nx.draw(my_chart, with_labels=True, font_weight='bold')
+
+
+my_crawl = Crawler('https://en.wikipedia.org/wiki/Router_(computing)', 1)
+for x in my_crawl.crawl():
+    print(x)
 
 
 
+# https://en.wikipedia.org/wiki/Router_(computing)
 
 
-
-
-# class Uri():
-#     """
-#     Uri Class
-#     """
+# my_file = []
+# for x in rate:
+#     my_file.append(x._json)
 #
-#     @staticmethod
-#     def new():
-#         return UriBuilder()
+# with open('data.json', 'w') as f:
+#     json.dump(my_file, f)
 #
-#     def __init__(self, scheme: str, host: str, port: int, path: str):
-#         self.scheme = scheme
-#         self.host = host
-#         self.port = port
-#         self.path = path
-#
-#     def getme(self):
-#         response = requests.get(self.to_string())
-#         return response.text
-#
-#         # return requests.get(self.to_string())
-#
-#     def to_string(self):
-#         # input = "&".join(f'{k}={v}' for k, v in self.qparam.items())
-#         return f'{self.scheme}://{self.host}:{self.port}/{self.path}'
-#
-#
-# class UriBuilder():
-#     """
-#     Build the web uri:  scheme, host, path and parameter
-#     """
-#
-#     def __init__(self, scheme: str = None, host: str = None, port: int = None, path: str = None):
-#         self.scheme = scheme
-#         self.host = host
-#         self.port = port
-#         self.path = path
-#
-#     def with_scheme(self, scheme):
-#         return UriBuilder(scheme=scheme, host=self.host, port=self.port, path=self.path)
-#
-#     def with_host(self, host):
-#         return UriBuilder(scheme=self.scheme, host=host, port=self.port, path=self.path)
-#
-#     def with_port(self, port):
-#         return UriBuilder(scheme=self.scheme, host=self.host, port=port, path=self.path)
-#
-#     def with_path(self, path):
-#         return UriBuilder(scheme=self.scheme, host=self.host, port=self.port, path=path)
-#
-#     def to_uri(self):
-#         return Uri(scheme=self.scheme, host=self.host, port=self.port, path=self.path)
-#
-#
-# def main():
-#     parser = argparse.ArgumentParser(description="Let's get some data son!")
-#     parser.add_argument('--scheme', type=str, help='Please enter a scheme (ex. http)')
-#     parser.add_argument('--host', type=str, help='Please enter a host (ex. demo.codingnomads.co)')
-#     parser.add_argument('--port', type=int, help='Please enter a port (ex. 8080)')
-#     parser.add_argument('--path', type=str, help='Please enter a host (ex. tasks_api/users)')
-#     args = parser.parse_args()
-#
-#     u = Uri.new().with_scheme(args.scheme).with_host(args.host).with_port(args.port).with_path(args.path)
-#     print(u.to_uri().to_string())
-#     print(u.to_uri().getme())
-#
-#
-# if __name__ == '__main__':
-#     main()
+# with open('data.json', 'r') as f:
+#     data = json.load(f)
